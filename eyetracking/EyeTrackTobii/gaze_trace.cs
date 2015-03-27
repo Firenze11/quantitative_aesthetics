791369f4098949d6f68e765b079d80107ebea30b
aiko-nakano <anakano09@gmail.com>
2015-03-27 19:29:59 -0400

    uploaded eyetracking folder with gaze data

---
+++ eyetracking/EyeTrackTobii/gaze_trace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace eyetrack
+{
+    class gaze_trace
+    {
+        public List<Point3d> trace = new List<Point3d>();
+        int MaxTraceCount = 10;
+
+        public void AddTrace(List<Point3d> trace)
+        {
+            foreach (Point3d pt in trace)
+            {
+                trace.Add(pt);
+            }
+            if (trace.Count > MaxTraceCount)
+                trace.RemoveAt(0);
+        }
+
+    }
+}

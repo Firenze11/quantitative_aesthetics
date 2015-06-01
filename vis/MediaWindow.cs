75ab0ad9930eb79fcc249991af85ff6c9ca9123b
Lezhi <lli2@gsd.harvard.edu>
2015-05-31 22:59:40 -0400

    finished vis code

--- vis/MediaWindow.cs
+++ vis/MediaWindow.cs
@@ -57,8 +57,8 @@ namespace testmediasmall
         static bool checkVideo = false;
         static bool playbackmode = false;
         public static bool multipleScreen = false;
-        static int maxframes = 1500;
-        static int minframes = 1500;
+        static int maxframes = 1200;
+        static int minframes = 1200;
 
         static int skippedFrameRange = 10;
         public static int screenCount = 3;
@@ -74,6 +74,9 @@ namespace testmediasmall
         //C_View is a class defined in the C_geometry.cs file and is just a helper object for managing viewpoint / tragetpoint camera mechanics
         public GLutils.C_View Viewer = new GLutils.C_View();
         static CvAnalysis CV = new CvAnalysis();
+        static ColorQuant ColorQuantizer = new ColorQuant();
+        Colormap initialCMap = new Colormap();
+        Colormap diffColorMap = new Colormap();
         static int frameNumber;
 
         //gaze property
@@ -373,22 +376,24 @@ namespace testmediasmall
                 }
             }
         }
-        
+
         void PlayBack()
         {
-            Screen.framecount++;
-            for (int i = 0; i < Screens.Count; i++)
-            {
-                Screens[i].OnTimeLapse(dpoint);
-            }
-            for (int i = 0; i < Screens.Count; i++)
-            {
-                Screens[i].FrameUpdate();
-            }
-            for (int i = 0; i < Screens.Count; i++)
-            {
-                if (Screens[i].ison) { Screens[i].DrawVbit(); }
-            }
+
+            //Screen.framecount++;
+            //for (int i = 0; i < Screens.Count; i++)
+            //{
+            //    Screens[i].OnTimeLapse(dpoint);
+            //}
+            //for (int i = 0; i < Screens.Count; i++)
+            //{
+            //    Screens[i].FrameUpdate();
+            //}
+            //for (int i = 0; i < Screens.Count; i++)
+            //{
+            //    if (Screens[i].ison) { Screens[i].DrawVbit(); }
+            //}
+
             //aaa++;
             //Console.WriteLine("aaa = "+aaa);
             //byte[, ,] pre_px = Vframe_repository[aaa].pix_data;
@@ -424,16 +429,10 @@ namespace testmediasmall
             VFrame vf = new VFrame();
             CV.FrameUpdate(px, rx, ry);   
 
-            //////////////////////////////////////////////////////////////////////////////color palette code
-            ColorQuant ColorQuantizer = new ColorQuant();
-            vf.initialCMap = ColorQuantizer.MedianCutQuantGeneral(Video, 16);
-            //Colormap initialCMap = ColorQuantizer.MedianCutQuantGeneral(vf, rx, ry, 16);    //sort by frequency
-            vf.DiffColorMap = ColorQuantizer.SortByDifference(vf.initialCMap);   //sort intialCMap again by the different; distinct color
-            //Colormap HueColorMap = ColorQuantizer.SortByHue(initialCMap);   //sort intialCMap by hue
+            vf.initialCMap = initialCMap;
+            vf.DiffColorMap = diffColorMap;
             var a = ColorQuantizer.TranslateHSV(vf.DiffColorMap[0]);
             vf.domiHue = a[0];
-            ////////////////////////////////////////////////////////////////////////end of color palette cod
-
             //double threshold = 5.0;
 
             int j2;
@@ -499,7 +498,7 @@ namespace testmediasmall
 
         public void OnFrameUpdate()  //executed 20d times per second.
         {
-            GL.ClearColor(0.0f, 0.6f, 0.6f, 1.0f);
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
@@ -515,6 +514,11 @@ namespace testmediasmall
 
                 if (Vframe_repository.Count <= maxframes) 
                 {
+                    //////////////////////////////////////////////////////////////////////////////color palette code
+                    initialCMap = ColorQuantizer.MedianCutQuantGeneral(Video, 16);
+                    diffColorMap = ColorQuantizer.SortByDifference(initialCMap);   //sort intialCMap again by the different; distinct color
+                    ////////////////////////////////////////////////////////////////////////end of color palette cod
+
                     BuildVfRepo();
                     if (Vframe_repository.Count > 1)
                     {
@@ -550,10 +554,198 @@ namespace testmediasmall
                 if (!CalibrationVideo.IsVideoCapturing) return;
                 if (CalibrationVideo.NeedUpdate) CalibrationVideo.UpdateFrame(true);
                 if (checkVideo) { DrawFullScreen (Video); }
-                else { DrawFullScreen (CalibrationVideo); }
+                //else { DrawFullScreen (CalibrationVideo); }
                 CalculateGaze();
+
+                //visualize video property data
+                bool visMotion = true;
+                bool visColor = false;
+                bool visHisto = false;
+                if (visMotion) { VisualizeMotion(); }
+                if (visColor) { VisualizeColor(); }
+                if (visHisto) { VisualizeHisto(); }
+
                 VisualizeGaze();
+
             }
+        }
+        
+        void VisualizeMotion()
+        {
+            VideoPixel[,] px = Video.Pixels;
+
+            for (int j = 0; j < ry; ++j)
+            {
+                for (int i = 0; i < rx; ++i)
+                {
+                    if (px[j, i].mx > 0 )
+                        GL.Color4(1.0, 1.0, 0.0, 0.5);
+                    else
+                        GL.Color4(0.0, 1.0, 1.0, 0.5);
+                    GL.Begin(PrimitiveType.Lines);
+                    GL.Vertex2(i, j);
+                    GL.Vertex2(i + px[j, i].mx * 10.0, j + px[j, i].my * 10.0);
+                    GL.End();
+                }
+            }
+        }
+
+        static double ColorDist(VideoPixel px, RGBA_Quad quad)
+        {
+            return Math.Sqrt((quad.R / 255.0 - px.R) * (quad.R / 255.0 - px.R)
+                             + (quad.G / 255.0 - px.G) * (quad.G / 255.0 - px.G)
+                             + (quad.B / 255.0 - px.B) * (quad.B / 255.0 - px.B));
+        }
+        static double ColorDist(byte[] px, RGBA_Quad quad)
+        {
+            return Math.Sqrt((quad.R - px[2]) * (quad.R - px[2])
+                             + (quad.G - px[1]) * (quad.G - px[1])
+                             + (quad.B - px[0]) * (quad.B - px[0]));
+        }
+
+        VBitmap recreateImg;
+        void VisualizeColor_notused()
+        {
+            if ( recreateImg == null) { recreateImg = new VBitmap(rx, ry); }
+            //byte[, ,] recreate_pix_data = new byte[ry, rx, 3];
+            //recreateImg.FromVideo(Video);
+
+            ColorQuant ColorQuantizer = new ColorQuant();
+            Colormap initialCMap = ColorQuantizer.MedianCutQuantGeneral(Video, 16);
+            Colormap diffColorMap = ColorQuantizer.SortByDifference(initialCMap);
+
+            double threshold = 0.1;
+            for (int j = 0; j < ry; j++)
+            {
+                for (int i = 0; i < rx; i++)
+                {
+                    double min = 2.0;
+                    int minId = -1;
+                    for (int k = 0; k < diffColorMap.Count; k++)
+                    {
+                        double dist = ColorDist(Video.Pixels[j, i], diffColorMap[k]);
+                        if (dist < threshold)
+                        {
+                            minId = k;
+                            break;
+                        }
+                        else
+                        {
+                            if (dist < min)
+                            {
+                                min = dist;
+                                minId = k;
+                            }
+                        }
+                    }
+                    if (minId < 3)
+                    {
+                        recreateImg.Pixels[j, i].R = diffColorMap[minId].R / 255.0;
+                        recreateImg.Pixels[j, i].G = diffColorMap[minId].G / 255.0;
+                        recreateImg.Pixels[j, i].B = diffColorMap[minId].B / 255.0;
+                    }
+                    else
+                    {
+                        recreateImg.Pixels[j, i].R = 0.0;
+                        recreateImg.Pixels[j, i].G = 0.0;
+                        recreateImg.Pixels[j, i].B = 0.0;
+                    }
+                }
+                recreateImg.Draw(0.0, 0.0, rx, ry, 1.0);
+            }
+        }
+
+        void VisualizeColor()
+        {
+            if (recreateImg == null) { recreateImg = new VBitmap(rx, ry); }
+            VideoPixel[,] px = Video.Pixels;
+
+            double threshold = 5.0;
+
+            int j2;
+            byte[,,] recreate_pix_data = new byte[ry, rx, 3];
+            for (int j = 0; j < ry; j++)
+            {
+                j2 = ry - j - 1;
+                for (int i = 0; i < rx; i++)
+                {
+                    double min = 442.0;
+                    int minId = -1;
+                    for (int k = 0; k < diffColorMap.Count; k++)
+                    {
+                        byte[] px_d = { CV.imgdataBGR[j2, i, 0], CV.imgdataBGR[j2, i, 1], CV.imgdataBGR[j2, i, 2] };
+                        double dist = ColorDist(px_d, diffColorMap[k]);
+                        if (dist < threshold)
+                        {
+                            minId = k;
+                            break;
+                        }
+                        else
+                        {
+                            if (dist < min)
+                            {
+                                min = dist;
+                                minId = k;
+                            }
+                        }
+                    }
+
+                    if (minId < 3)
+                    {
+                        recreate_pix_data[j, i, 2] = diffColorMap[minId].R;
+                        recreate_pix_data[j, i, 1] = diffColorMap[minId].G;
+                        recreate_pix_data[j, i, 0] = diffColorMap[minId].B;
+                    }
+                    else
+                    {
+                        recreate_pix_data[j, i, 2] = 0;
+                        recreate_pix_data[j, i, 1] = 0;
+                        recreate_pix_data[j, i, 0] = 0;
+                    }
+                }
+            }
+            recreateImg.FromFrame(recreate_pix_data);
+            recreateImg.Update();
+            recreateImg.Draw(0, 0, rx, ry, 1.0);
+
+            for (int i = 0; i < diffColorMap.Count; i++)
+            {
+                //visualize the palette
+                RGBA_Quad quad = diffColorMap[i];
+                GL.PointSize(40);
+                GL.Color4(quad.R / 255.0, quad.G / 255.0, quad.B / 255.0, 1.0);
+                GL.Begin(PrimitiveType.Points);
+                GL.Vertex2(10 + i * 5, 30);
+                GL.End();
+            }
+            GL.End();
+        }
+
+        void VisualizeHisto()
+        {
+            GL.Color4(1.0, 0.0, 0.0, 1.0);
+            GL.Begin(PrimitiveType.LineStrip);
+            for (int i = 0; i < CV.HistoRA.Length; ++i)
+            {
+                GL.Vertex2(i * 30.0, (double)CV.HistoRA[i] / 500.0);
+            }
+            GL.End();
+
+            GL.Color4(0.0, 1.0, 0.0, 1.0);
+            GL.Begin(PrimitiveType.LineStrip);
+            for (int i = 0; i < CV.HistoGA.Length; ++i)
+            {
+                GL.Vertex2(i * 30.0, (double)CV.HistoGA[i] / 500.0);
+            }
+            GL.End();
+
+            GL.Color4(0.0, 0.0, 1.0, 1.0);
+            GL.Begin(PrimitiveType.LineStrip);
+            for (int i = 0; i < CV.HistoBA.Length; ++i)
+            {
+                GL.Vertex2(i * 30.0, (double)CV.HistoBA[i] / 500.0);
+            }
+            GL.End();
         }
     }
 }
